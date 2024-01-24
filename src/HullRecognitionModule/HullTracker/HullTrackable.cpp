5f50c1b6387a8d9afb4194ceb56402607ebf0766
Isidro Butaslac <butaslaciii@gmail.com>
2024-01-24 16:39:12 +0800

    clean HullTrackable

--- src/HullRecognitionModule/HullTracker/HullTrackable.cpp
+++ src/HullRecognitionModule/HullTracker/HullTrackable.cpp
@@ -1,60 +1,78 @@
 #include "HullTrackable.h"
 
-HullTrackable::HullTrackable(int id, const std::vector<cv::Point>& hull)
+HullTrackable::HullTrackable(int id, const std::vector<cv::Point>& initHull)
     : trackableId(id)
-    , hullPoints(hull)
-    , framesSinceSeen(0)
-    , hullCentroid(0, 0)
-    , isCentroidCalculated(false)
-    , averageSpeed(0.0)
+    , hullPoints(initHull)
     , trackingStartPoint(computeCentroid(hullPoints))
     , trackingEndPoint(0, 0)
+    , framesSinceSeen(0)
+    , averageSpeed(0.0)
+    , hullCentroid(0, 0)
+    , isCentroidCalculated(false)
     , fpsHelper()
 {
     fpsHelper.startSample();
 }
 
+/**
+ * @brief Gets the trackable object's ID.
+ * @return The ID of the trackable object.
+ */
 int HullTrackable::getTrackableId() const
 {
     return trackableId;
 }
 
-const std::vector<cv::Point>& HullTrackable::getHullPoints() const
-{
-    return hullPoints;
-}
-
+/**
+ * @brief Calculates and returns the area of the trackable object's hull.
+ * @return The area of the hull.
+ */
 float HullTrackable::getHullArea() const
 {
     return cv::contourArea(hullPoints);
 }
 
+/**
+ * @brief Gets the current hull points of the trackable object.
+ * @return A constant reference to the vector of hull points.
+ */
+const std::vector<cv::Point>& HullTrackable::getHullPoints() const
+{
+    return hullPoints;
+}
+
+/**
+ * @brief Gets the number of frames since the object was last seen.
+ * @return The number of frames since last seen.
+ */
+int HullTrackable::getFramesSinceSeen() const
+{
+    return framesSinceSeen;
+}
+
+/**
+ * @brief Sets the number of frames since the trackable object was last seen.
+ * @param untrackedFrames The number of frames the object was untracked.
+ */
+void HullTrackable::setFramesSinceSeen(int untrackedFrames)
+{
+    framesSinceSeen = untrackedFrames;
+}
+
+/**
+ * @brief Updates the hull points of the trackable object.
+ * @param newHull The new set of hull points.
+ */
 void HullTrackable::setHullPoints(const std::vector<cv::Point>& newHull)
 {
     hullPoints = newHull;
     isCentroidCalculated = false;
 }
 
-int HullTrackable::getFramesSinceSeen() const
-{
-    return framesSinceSeen;
-}
-
-void HullTrackable::setFramesSinceSeen(int untrackedFrames)
-{
-    framesSinceSeen = untrackedFrames;
-}
-
-cv::Point2f HullTrackable::calculateCentroid() const
-{
-    if(!isCentroidCalculated)
-    {
-        hullCentroid = computeCentroid(hullPoints);
-        isCentroidCalculated = true;
-    }
-    return hullCentroid;
-}
-
+/**
+ * @brief Calculates the average speed of the trackable object based on its movement.
+ * @return The average speed of the object, in pixels/second
+ */
 float HullTrackable::calculateAverageSpeed()
 {
     float travelTime = fpsHelper.endSample() / 1000.0;
@@ -65,6 +83,26 @@ float HullTrackable::calculateAverageSpeed()
     return averageSpeed;
 }
 
+/**
+ * @brief Lazily calculates and returns the centroid of the trackable object.
+ * @return The centroid of the hull.
+ */
+cv::Point2f HullTrackable::calculateCentroid() const
+{
+    if(!isCentroidCalculated)
+    {
+        hullCentroid = computeCentroid(hullPoints);
+        isCentroidCalculated = true;
+    }
+    return hullCentroid;
+}
+
+/**
+ * @brief Static method to compute the centroid of a given hull.
+ * This can also be used outside the HullTrackable Class, e.g. HullTracker.
+ * @param hull The hull for which to compute the centroid.
+ * @return The centroid points (x, y) of the given hull.
+ */
 cv::Point2f HullTrackable::computeCentroid(const std::vector<cv::Point>& hull)
 {
     if(hull.empty())
--- src/HullRecognitionModule/HullTracker/HullTrackable.h
+++ src/HullRecognitionModule/HullTracker/HullTrackable.h
@@ -3,34 +3,46 @@
 
 #include "FPSHelper.h"
 #include <opencv2/opencv.hpp>
+#include <vector>
 
+/**
+ * @brief A HullTrackable object with a specified ID and hull shape.
+ * @param id The unique identifier for the trackable object.
+ * @param initHull The initial hull points of the object.
+ */
 class HullTrackable
 {
 public:
-    HullTrackable(int id, const std::vector<cv::Point>& hull);
+    HullTrackable(int id, const std::vector<cv::Point>& initHull);
 
     int getTrackableId() const;
-    const std::vector<cv::Point>& getHullPoints() const;
-    void setHullPoints(const std::vector<cv::Point>& newHull);
     float getHullArea() const;
-    cv::Point2f calculateCentroid() const;
-    float calculateAverageSpeed();
+    const std::vector<cv::Point>& getHullPoints() const;
 
     int getFramesSinceSeen() const;
+
     void setFramesSinceSeen(int frames);
+    void setHullPoints(const std::vector<cv::Point>& newHull);
+
+    float calculateAverageSpeed();
+    cv::Point2f calculateCentroid() const;
 
     static cv::Point2f computeCentroid(const std::vector<cv::Point>& hull);
 
 private:
-    FPSHelper fpsHelper;
     const int trackableId;
+
     std::vector<cv::Point> hullPoints;
-    int framesSinceSeen;
-    mutable cv::Point2f hullCentroid;
-    mutable bool isCentroidCalculated;
-    float averageSpeed;
     cv::Point2f trackingStartPoint;
     cv::Point2f trackingEndPoint;
+
+    int framesSinceSeen;
+    float averageSpeed;
+
+    mutable cv::Point2f hullCentroid;
+    mutable bool isCentroidCalculated;
+
+    FPSHelper fpsHelper;
 };
 
 #endif
