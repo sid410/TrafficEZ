94e5d888c17936d4710c7056a0d474164cf08963
jeanmaureal07 <jean.maureal@pointersbit.com>
2024-12-12 12:07:34 +0800

    doc: updated script to adhere to update on deprecated apt-get

--- scripts/add_apt_keys.sh
+++ scripts/add_apt_keys.sh
@@ -11,10 +11,20 @@ keys=(
     "3B4FE6ACC0B21F32"
 )
 
+temp_keyring="/tmp/temp-keyring.gpg"
+
+echo "Creating a temporary keyring at $temp_keyring"
+touch "$temp_keyring"
+
 for key in "${keys[@]}"; do
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys "$key"
+    echo "Fetching and adding key: $key"
+    gpg --no-default-keyring --keyring "$temp_keyring" --keyserver keyserver.ubuntu.com --recv-keys "$key"
+    gpg --no-default-keyring --keyring "$temp_keyring" --export "$key" | sudo tee /etc/apt/trusted.gpg.d/"$key".gpg > /dev/null
 done
 
-sudo apt-get update
+echo "Cleaning up temporary keyring"
+rm -f "$temp_keyring"
+
+sudo apt update
 
 echo "##### Keys Added and Packages Updated #####"
