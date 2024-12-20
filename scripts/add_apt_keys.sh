e95fe4bbbb0f03b3b95fdc66fab5da7e907917ce
jeanmaureal07 <jean.maureal@pointersbit.com>
2024-12-20 15:24:07 +0800

    Revert "fix: update script"
    This reverts commit 7e1364bd3a894824627331c56bd9911e95c6f696.

--- scripts/add_apt_keys.sh
+++ scripts/add_apt_keys.sh
@@ -11,10 +11,19 @@ keys=(
     "3B4FE6ACC0B21F32"
 )
 
+temp_keyring="/tmp/temp-keyring.gpg"
+
+echo "Creating a temporary keyring at $temp_keyring"
+touch "$temp_keyring"
+
 for key in "${keys[@]}"; do
     echo "Fetching and adding key: $key"
-    curl -fsSL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x$key" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/"$key".gpg > /dev/null
+    gpg --no-default-keyring --keyring "$temp_keyring" --keyserver keyserver.ubuntu.com --recv-keys "$key"
+    gpg --no-default-keyring --keyring "$temp_keyring" --export "$key" | sudo tee /etc/apt/trusted.gpg.d/"$key".gpg > /dev/null
 done
+
+echo "Cleaning up temporary keyring"
+rm -f "$temp_keyring"
 
 sudo apt update
 
